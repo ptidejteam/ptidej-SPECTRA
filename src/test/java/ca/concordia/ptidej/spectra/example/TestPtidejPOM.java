5539f88140e8bbd8ec784b233d3a06b5fdb10658
Rushin-Makwana <rushymakwana@gmail.com>
2025-11-14 17:52:21 -0500

    Removed timing related code to exit after specific time

--- src/test/java/ca/concordia/ptidej/spectra/example/TestPtidejPOM.java
+++ src/test/java/ca/concordia/ptidej/spectra/example/TestPtidejPOM.java
@@ -8,7 +8,7 @@ import org.noureddine.joularjx.result.ResultWriter;
 import java.io.File;
 import java.io.IOException;
 
-public class TestPtidejPomCore {
+public class TestPtidejPOM {
 
     @Test
     public void testOverload() throws IOException {
@@ -16,12 +16,12 @@ public class TestPtidejPomCore {
         ResultWriter writer = new CsvResultWriter();
         final Launcher launcher = new Launcher();
 
-        String ptidejClasspath = "../POM/target/test-classes:../POM/target/pom-core-1.0.0-tests.jar"
+        String classpath = "../POM/target/test-classes:../POM/target/pom-core-1.0.0-tests.jar"
                 + File.pathSeparator + "../POM/target/pom-core-1.0.0-jar-with-dependencies.jar";
 
         launcher.launch(
                 writer,
-                ptidejClasspath, "org.junit.runner.JUnitCore",
+                classpath, "org.junit.runner.JUnitCore",
                         "pom.test.TestPOM"
         );
 
